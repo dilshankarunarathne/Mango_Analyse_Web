4dbf0bd92653c30b0a68e13d9194d26b81be2363
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2023-11-09 14:29:17 +0530

    02

--- mango-analysis/src/App.js
+++ mango-analysis/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
-
-
+import ResultPage from './Components/results';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-   
-    </div>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<ResultPage />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
